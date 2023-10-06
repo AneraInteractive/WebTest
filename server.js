79124de5382349a271f578bc62968e09fd88d005
Tabea van Hasz <65543541+DeepThoughtXLII@users.noreply.github.com>
2023-10-06 15:52:39 +0200

    changed folder structure

--- server.js
+++ server.js
@@ -1,41 +1,73 @@
-const http = require('http')
-const fs = require('fs')
-const port = 3000
 
-const server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type' : 'text/html' })
-    fs.readFile('index1.html', function (error, data){
-        if(error){
-            res.writeHead(404)
-            res.write('Error: File Not Found')
-        }else{
-            res.write(data)
-        }
-        res.end()
-    })
+
+const gameIds = new Map();
+const clients = new Map();
+
+const WebSocket = require('ws');
+const port = 8080;
+let id = 0;
+
+const wss = new WebSocket.Server({port}, ()=>{
+    console.log('server started');
 })
 
+wss.on('connection', function connection(client){
+    client.send(CreateNewID().toString());
 
-server.listen(port, function(error){
-if(error){
-    console.log('something went wrong', error)
-}else{
-    console.log('server is listening on port ' + port)
+    client.on('message', (msg)=>{
+        console.log('data received %o' +msg.toString());       
+        try{
+            const parsedMsg = JSON.parse(msg.toString()); //Letter object sender: id, text: message, sharedID: game id
+            BroadcastToClientsInGame(parsedMsg.sharedID, parsedMsg.text);
+            if(!clients.has(parsedMsg.sender)){ //Check if we have saved the client already
+                clients.set(parsedMsg.sender, client); //save client
+                console.log('saved client with id: '+ parsedMsg.sender + "and client: " + client);
+                AddParticipantToGame(parsedMsg.sharedID, parsedMsg.sender);
+            }
+            //   GetClientsInGame(parsedMsg.sharedID).forEach(function(clientConnection, clientId) {
+            //     clientConnection.send(parsedMsg.text);
+            //     console.log("id " + clientId + "    connection " + clientConnection);
+            //   });
+        }catch(error){
+            console.log(error.message);
+        }
+    });
+});
+
+wss.on('listening', ()=>{
+    console.log('server is listening on port: ' + port);
+})
+
+function AddParticipantToGame(gameId, senderId){
+    if(!gameIds.has(gameId)){
+        console.log("game with id: "+ gameId +" was created");
+        const gameArr = [];
+        gameIds.set(gameId, gameArr);
+    }
+    gameIds.get(gameId).push(senderId);
+    console.log("added " + senderId + " to game " + gameId);
 }
 
-})
+function BroadcastToClientsInGame(gameId, message){
+    if(gameIds.has(gameId) && gameIds.get(gameId).length > 1){
+        const gameClients = new Map();
+        for(let clientId of gameIds.get(gameId)){
+            gameClients.set(clientId, clients.get(clientId));
+            console.log("ingame client is client: " + clientId + " with connection " + gameClients.get(clientId));
+        }
+        gameClients.forEach(function(clientConnection, clientId) {
+            clientConnection.send(message);
+            console.log("message sent to" + clientId);
+        });
+    }
+}
 
-// websocket.onopen = function (e) {
+function CreateNewID(){
+    let newId = id;
+    id++;
+    return newId;
+}
 
-// };
 
-// websocket.onclose = function (e) {
 
-// };
 
-// websocket.onmessage = function (e) {
-
-// };
-
-// websocket.onerror = function (e) {
-// };
